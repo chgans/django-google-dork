ccf1cd8954d8c0428d38d29d5990f999ace1a482
Christian Gagneraud <chgans@gna.org>
2014-11-16 12:03:30 +1300

    Initial tests

---
+++ tests/runtests.py
@@ -0,0 +1,80 @@
+import os, sys
+
+import django
+from django.conf import settings
+from django.core.management import execute_from_command_line
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(os.path.join('..', BASE_DIR))
+
+CUSTOM_INSTALLED_APPS = (
+    'google_dork',
+)
+
+ALWAYS_INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+)
+
+ALWAYS_MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+
+settings.configure(
+    SECRET_KEY="django_tests_secret_key",
+    DEBUG=False,
+    TEMPLATE_DEBUG=False,
+    ALLOWED_HOSTS=[],
+    INSTALLED_APPS=ALWAYS_INSTALLED_APPS + CUSTOM_INSTALLED_APPS,
+    MIDDLEWARE_CLASSES=ALWAYS_MIDDLEWARE_CLASSES,
+    ROOT_URLCONF='tests.urls',
+    DATABASES={
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+        }
+    },
+    LANGUAGE_CODE='en-us',
+    TIME_ZONE='UTC',
+    USE_I18N=True,
+    USE_L10N=True,
+    USE_TZ=True,
+    STATIC_URL='/static/',
+    # Use a fast hasher to speed up tests.
+    PASSWORD_HASHERS=(
+        'django.contrib.auth.hashers.MD5PasswordHasher',
+    ),
+)
+
+django.setup()
+args = [sys.argv[0], 'test']
+path = os.path.dirname('./')
+offset = 1
+
+# Allow accessing test options from the command line.
+try:
+    sys.argv[1]
+except IndexError:
+    pass
+else:
+    option = sys.argv[1].startswith('-')
+    if not option:
+        path = os.path.join(path, sys.argv[1])
+        offset = 2
+
+# The second argument points to the current directory.
+args.append(path)
+# ``verbosity`` can be overwritten from command line.
+args.append('--verbosity=2')
+args.extend(sys.argv[offset:])
+
+execute_from_command_line(args)
---
+++ tests/tests.py
@@ -0,0 +1,178 @@
+from django.test import TestCase
+from django.db import IntegrityError
+from django.core.exceptions import ValidationError
+
+from google_dork.models import Campaign, Dork, Run, Result
+
+class TestCampaign(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_name_not_empty(self):
+        with self.assertRaises(ValidationError):
+            c=Campaign.objects.create(name="")
+            c.full_clean()
+
+    def test_name_is_stripped(self):
+        c=Campaign.objects.create(name=" test ")
+        c.full_clean()
+        self.assertEqual(c.name, "test")
+
+    def test_name_is_long_enough(self):
+        with self.assertRaises(ValidationError):
+            c=Campaign.objects.create(name="t"*3)
+            c.full_clean()
+
+    def test_name_is_short_enough(self):
+        with self.assertRaises(ValidationError):
+            c=Campaign.objects.create(name="t"*33)
+            c.full_clean()
+
+    def test_name_is_unique(self):
+        with self.assertRaises(IntegrityError):
+            Campaign.objects.create(name="test")
+            Campaign.objects.create(name="test")
+
+    def test_run_set(self):
+        c=Campaign.objects.create(name="campaign")
+        d=Dork.objects.create(campaign=c, query="query")
+        n=3
+        for i in range(n):
+            r=Run.objects.create(dork=d)
+        self.assertEqual(c.run_set.count(), n)
+        self.assertEqual(c.run_set.last(), r)
+        self.assertEqual(c.run_set.last(), d.run_set.last())
+
+    def test_result_set(self):
+        c=Campaign.objects.create(name="campaign")
+        d=Dork.objects.create(campaign=c, query="query")
+        r=Run.objects.create(dork=d)
+        n=3
+        for i in range(n):
+            s=Result.objects.create(title="title%d" % (i),
+                                    summary="summary",
+                                    url="http://some.where/path")
+            r.result_set.add(s)
+        self.assertEqual(c.result_set.count(), n)
+        self.assertEqual(c.result_set.last(), s)
+        self.assertEqual(c.result_set.last(), r.result_set.last())
+
+class TestDork(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_campaign_not_null(self):
+        with self.assertRaises(IntegrityError):
+            d=Dork.objects.create()
+            d.full_clean()
+
+    def test_query_not_null(self):
+        c=Campaign.objects.create(name="c")
+        with self.assertRaises(IntegrityError):
+            d=Dork.objects.create(campaign=c, query=None)
+
+    def test_query_not_empty(self):
+        c=Campaign.objects.create(name="c")
+        with self.assertRaises(ValidationError):
+            d=Dork.objects.create(campaign=c, query="")
+            d.full_clean()
+
+    def test_query_is_stripped(self):
+        c=Campaign.objects.create(name="c")
+        d=Dork.objects.create(campaign=c, query=" test ")
+        d.full_clean()
+        self.assertEqual(d.query, "test")
+
+    def test_query_is_long_enough(self):
+        c=Campaign.objects.create(name="c")
+        with self.assertRaises(ValidationError):
+            d=Dork.objects.create(campaign=c, query="t"*0)
+            d.full_clean()
+
+    def test_query_is_short_enough(self):
+        c=Campaign.objects.create(name="c")
+        with self.assertRaises(ValidationError):
+            d=Dork.objects.create(campaign=c, query="t"*257)
+            d.full_clean()
+
+    def test_campaign_and_query_00(self):
+        c0=Campaign.objects.create(name="c0")
+        c1=Campaign.objects.create(name="c1")
+        Dork.objects.create(campaign=c0, query="test0")
+        Dork.objects.create(campaign=c1, query="test1")
+
+    def test_campaign_and_query_01(self):
+        c0=Campaign.objects.create(name="c0")
+        c1=Campaign.objects.create(name="c1")
+        Dork.objects.create(campaign=c0, query="test")
+        Dork.objects.create(campaign=c1, query="test")
+
+    def test_campaign_and_query_10(self):
+        c=Campaign.objects.create(name="c")
+        Dork.objects.create(campaign=c, query="test0")
+        Dork.objects.create(campaign=c, query="test1")
+
+    def test_campaign_and_query_11(self):
+        c=Campaign.objects.create(name="c")
+        Dork.objects.create(campaign=c, query="test")
+        with self.assertRaises(IntegrityError):
+            Dork.objects.create(campaign=c, query="test")
+
+    def test_result_set(self):
+        c=Campaign.objects.create(name="campaign")
+        d=Dork.objects.create(campaign=c, query="query")
+        r=Run.objects.create(dork=d)
+        for i in range(3):
+            s=Result.objects.create(title="title%d" % i,
+                                    summary="summary",
+                                    url="http://some.where/path")
+            r.result_set.add(s)
+        self.assertEqual(d.result_set.count(), 3)
+        self.assertEqual(d.result_set.last(), s)
+        self.assertEqual(d.result_set.last(), r.result_set.last())
+
+class TestRun(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_create_dork_not_empty(self):
+        with self.assertRaises(IntegrityError):
+            Run.objects.create()
+
+    def test_create_wo_results(self):
+        c=Campaign.objects.create(name="campaign")
+        d=Dork.objects.create(campaign=c, query="query")
+        r=Run.objects.create(dork=d)
+
+    # result_set doen't contains 2 identical result
+    # This is actually enforced by Django itself!
+    def test_result_uniqness(self):
+        c=Campaign.objects.create(name="campaign")
+        d=Dork.objects.create(campaign=c, query="query")
+        r=Run.objects.create(dork=d)
+        s=Result.objects.create(title="title",
+                                summary="summary",
+                                url="http://some.where/path")
+        r.result_set.add(s)
+        r.result_set.add(s)
+        self.assertEqual(r.result_set.count(), 1)
+
+class TestResult(TestCase):
+
+    def setUp(self):
+        pass
+
+    def create_result(self):
+        r=Result.objects.create(title="title",
+                                 summary="summary",
+                                 url="http://some.where/path")
+        return r
+
+    def test_uniqness(self):
+        r=self.create_result()
+        with self.assertRaises(IntegrityError):
+            self.create_result()
+            
